143cf5aa2d1946018095630fc45b5ec7834fd7c3
Asus <Asus@DESKTOP-2DDLAUN>
2025-04-25 14:21:27 +0300

    added Repository

---
+++ WatchList.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WatchList.DataAccess.Abstract
+{
+    public interface IRepository<T> where T : class // T bir sınıftır diyoruz, imzaları burada atıyoruz içeriğini GenericRepository de dolduracağız
+    {
+        List<T> GetList();
+
+        T GetByFilter(Expression<Func<T, bool>> predicate); //şarta göre tek bir değer getirme
+
+        T GetById(int id);
+
+        void Create(T entity);
+
+        void Update(T entity);
+
+        void Delete(int id);
+
+        int Count();
+
+        int FilteredCount(Expression<Func<T,bool>> predicate); //şarta göre count getirme
+
+        List<T> GetFilteredList(Expression<Func<T,bool>> predicate); //şarta göre listeleme
+
+    }
+}
---
+++ WatchList.DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WatchList.DataAccess.Abstract;
+using WatchList.DataAccess.Context;
+
+namespace WatchList.DataAccess.Repositories
+{
+    public class GenericRepository<T>(WatchListContext _context) : IRepository<T> where T : class //2- (WatchListContext _context) primery constarcter yapısı; yeni yöntem
+    {
+        //1-private readonly WatchListContext _context; // ctrl. generic constracter; eski yöntem
+        public DbSet<T> Table { get => _context.Set<T>(); }
+        public int Count()
+        {
+            return Table.Count();
+        }
+
+        public void Create(T entity) //Tabloya eklemem
+        {
+            Table.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entitiy = Table.Find(id);
+            Table.Remove(entitiy);
+            _context.SaveChanges();
+        }
+
+        public int FilteredCount(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).Count(); //Where içine predicate şart
+        }
+
+        public T GetByFilter(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).FirstOrDefault(); // tek bir değer
+        }
+
+        public T GetById(int id)
+        {
+            return Table.Find(id);
+        }
+
+        public List<T> GetFilteredList(Expression<Func<T, bool>> predicate)
+        {
+            return Table.Where(predicate).ToList();
+        }
+
+        public List<T> GetList()
+        {
+            return Table.ToList();
+        }
+
+        public void Update(T entity)
+        {
+            Table.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
