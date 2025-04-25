3eb8c965b6132dd71a414fbf5df0c79e2f3fd232
Asus <Asus@DESKTOP-2DDLAUN>
2025-04-25 13:13:46 +0300

    Added referance and package Added context class

---
+++ WatchList.DataAccess/Context/WatchListContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WatchList.Entity.Entity;
+
+namespace WatchList.DataAccess.Context
+{
+    public class WatchListContext : DbContext
+    {
+        public WatchListContext(DbContextOptions options): base(options)//appsettings'de tutmak için base     
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Series> Series { get; set; }
+
+        public DbSet<MovieList> MovieLists { get; set; }
+        public DbSet<MovieListItem> MovieListItems { get; set; }
+
+        public DbSet<SeriesList> SeriesLists { get; set; }
+        public DbSet<SeriesListItem> SeriesListItems { get; set; }
+
+        public DbSet<TierList> TierLists { get; set; }
+        public DbSet<TierListItem> TierListItems { get; set; }
+
+        public DbSet<Like> Likes { get; set; }
+
+
+    }
+}
