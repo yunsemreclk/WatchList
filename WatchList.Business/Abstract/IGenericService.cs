28fc1781c5b28856ed0b6af492de4329c77f5e4d
Asus <Asus@DESKTOP-2DDLAUN>
2025-04-25 17:43:58 +0300

    Business layer created

---
+++ WatchList.Business/Abstract/IGenericService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchList.Business.Absract
+{
+    public interface IGenericService<T> where T : class
+    {
+        List<T> TGetList();
+
+        T TGetByFilter(Expression<Func<T, bool>> predicate); //şarta göre tek bir değer getirme
+
+        T TGetById(int id);
+
+        void TCreate(T entity);
+
+        void TUpdate(T entity);
+
+        void TDelete(int id);
+
+        int TCount();
+
+        int TFilteredCount(Expression<Func<T, bool>> predicate); //şarta göre count getirme
+
+        List<T> TGetFilteredList(Expression<Func<T, bool>> predicate); //şarta göre listeleme
+    }
+}
