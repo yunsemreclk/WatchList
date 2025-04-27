04d320076c71fbb06db31bbf058ed0a0abc1b2d8
Asus <Asus@DESKTOP-2DDLAUN>
2025-04-27 22:17:25 +0300

    admin side movie adding deletion update process completed

--- WatchList.WebUI/Areas/Admin/Controllers/MovieController.cs
+++ WatchList.WebUI/Areas/Admin/Controllers/MovieController.cs
@@ -15,5 +15,45 @@ namespace WatchList.WebUI.Areas.Admin.Controllers
             var values = await _client.GetFromJsonAsync<List<ListMovieDto>>("movies");
             return View(values);
         }
+
+        public async Task<IActionResult> DeleteMovie(int id) 
+        {
+            await _client.DeleteAsync($"movies/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CreateMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMovie(CreateMovieDto createMovieDto)
+        {
+            await _client.PostAsJsonAsync("movies", createMovieDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdateMovie(int id)
+        {
+            var values = await _client.GetFromJsonAsync<UpdateMovieDto>($"movies/{id}");
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateMovie(UpdateMovieDto updateMovieDto)
+        {
+            await _client.PutAsJsonAsync("movies", updateMovieDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
+
+
+
     }
 }
